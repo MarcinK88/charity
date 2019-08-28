99a6bb85a1104428dad6b2ed967251d5b16abb2a
Marcin <marcin3216@gmail.com>
2019-08-28 09:40:27 +0200

    institution list

--- src/main/java/pl/coderslab/charity/Controllers/HomeController.java
+++ src/main/java/pl/coderslab/charity/Controllers/HomeController.java
@@ -1,16 +1,27 @@
 package pl.coderslab.charity.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.Models.Institution;
+import pl.coderslab.charity.Repositories.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private InstitutionRepository institutionRepository;
 
     @RequestMapping("/")
     public String homeAction(Model model){
+
+        List<Institution> intitutions = institutionRepository.findAll();
+        model.addAttribute("institutions", intitutions);
+
         return "index";
     }
 }
