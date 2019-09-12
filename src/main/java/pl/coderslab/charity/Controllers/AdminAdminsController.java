c4f42e61cde05edb946715f824699c5c876369ea
Marcin <marcin3216@gmail.com>
2019-09-12 14:28:54 +0200

    admins edit delete

--- src/main/java/pl/coderslab/charity/Controllers/AdminAdminsController.java
+++ src/main/java/pl/coderslab/charity/Controllers/AdminAdminsController.java
@@ -1,9 +1,14 @@
 package pl.coderslab.charity.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.coderslab.charity.Models.User;
 import pl.coderslab.charity.Services.DonationService;
 import pl.coderslab.charity.Services.InstitutionService;
 import pl.coderslab.charity.Services.UserRolesService;
@@ -47,6 +52,35 @@ public class AdminAdminsController {
         model.addAttribute("admins", userService.findAllByUserRoles(userRolesService.findByRole("ADMIN")));
 
         return "admin-admins";
+    }
+
+    @GetMapping("/admin/admins/edit/{id}")
+    public String editAdmin(@PathVariable long id, Model model) {
+        model.addAttribute("admin", userService.findById(id));
+        model.addAttribute("oldUsername", userService.findById(id).getUsername());
+        return "admin-admin-edit";
+    }
+
+    @PostMapping("/admin/admins/edit/{id}")
+    public String editUserPost(@ModelAttribute User user, @Param("oldUsername") String oldUsername) {
+
+        userService.update(user, oldUsername);
+
+        return "redirect:/admin/admins";
+    }
+
+    @GetMapping("/admin/admins/delete/{id}")
+    public String deleteUser(@PathVariable long id, Model model) {
+        model.addAttribute("user", userService.findById(id));
+
+        return "admin-admins-delete-confirm";
+    }
+
+    @PostMapping("/admin/admins/delete/{id}")
+    public String deleteUserPost(@ModelAttribute User user) {
+        userService.delete(user);
+
+        return "redirect:/admin/users";
     }
 
 }
